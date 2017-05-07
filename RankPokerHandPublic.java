34cb45a9d0be115591ecf3c39bc026eb425c1de4
Mable Johnson <mablejohnson492@gmail.com>
2017-05-08 00:32:05 +0200

    range vs range

--- RankPokerHandPublic.java
+++ RankPokerHandPublic.java
@@ -250,6 +250,17 @@ public class RankPokerHandPublic {
         runTime = System.currentTimeMillis() - startTime;
         printHandToCount();
         System.out.println(runTime + " ms");
+
+        System.out.println("AK vs QQ preflop:");
+        System.out.println(rangeVsRange(new int[] {12,11,-1,-1,-1,-1,-1,10,10}, new int[] {0,1,-1,-1,-1,-1,-1,0,2}));
+        System.out.println("AK vs QQ with flop: AKQ:");
+        System.out.println(rangeVsRange(new int[] {12,11,12,11,10,-1,-1,10,10}, new int[] {0,1,1,0,1,-1,-1,0,2}));
+        System.out.println("Pair of A on the flow vs random opponent");
+        System.out.println(rangeVsRange(new int[] {5,12,12,1,3,-1,-1,-1,-1}, new int[] {0,0,1,1,2,-1,-1,-1,-1}));
+        //System.out.println("Pair of 10's preflop against one player with random cards:"); // takes too long
+        //System.out.println(rangeVsRange(new int[] {8,8,-1,-1,-1,-1,-1,-1,-1}, new int[] {0,1,-1,-1,-1,-1,-1,-1,-1}));
+        //System.out.println("All two-player combinations"); // takes almost forever
+        //System.out.println(rangeVsRange(new int[] {8,8,-1,-1,-1,-1,-1,-1,-1}, new int[] {0,1,-1,-1,-1,-1,-1,-1,-1}));
     }
 
     private static int[] suits(int[] is) {
@@ -340,4 +351,235 @@ public class RankPokerHandPublic {
         }
     }
 
+    /**
+     * cards[0] = player 1 first hole card (0..12)
+     * suits[0] = player 1 first hole suit (0..3)
+     * cards[1] = player 1 second hole card (0..12)
+     * suits[1] = player 1 second hole suit (0..3)
+     * cards[i>=2, i<=6] = card on flop/turn/river (0..12) or -1 of not dealt yet
+     * cards[i>=2, i<=6] = suit on flop/turn/river (0..12) or -1 of not dealt yet
+     * cards[7] = player 2 first hole card (0..12) or -1 if unknown
+     * suits[7] = player 2 first hole suit (0..3) or -1 if unknown
+     * cards[8] = player 2 second hole card (0..12) or -1 if unknown
+     * suits[8] = player 2 second hole suit (0..3) or -1 if unknown
+     *
+     * for any i (0<=i<=8): if cards[i]==-1 then it must be suits[i]==-1
+     */
+    public static RangeResult rangeVsRange(int[] cards, int[] suits) {
+        RangeResult rangeResult = new RangeResult();
+        int[] nr = new int[7];
+        int[] suit = new int[7];
+        for (int i=0; i<7; i++) {
+            nr[i] = -1;
+            suit[i] = -1;
+        }
+        int newNr, newSuit;
+        for (int card1=0; card1<52 || cards[0] != -1; card1++) {
+            newNr = cards[0] != -1 ? cards[0] : card1 / 4;
+            newSuit = suits[0] != -1 ? suits[0] : card1 % 4;
+            if (cards[0] == -1 && duplicate(newNr, newSuit, nr, suit)) {
+                if (cards[0] != -1) {
+                    break;
+                }
+                continue;
+            }
+            nr[0] = newNr; int keepCard1 = newNr;
+            suit[0] = newSuit; int keepSuit1 = newSuit;
+            for (int card2 = 0; card2 < 52 || cards[1] != -1; card2++) {
+                if (cards[1] == -1 && cards[0] == -1 && card2 <= card1) {
+                    continue;
+                }
+                newNr = cards[1] != -1 ? cards[1] : card2 / 4;
+                newSuit = suits[1] != -1 ? suits[1] : card2 % 4;
+                if (duplicate(newNr, newSuit, nr, suit) || (cards[1] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                    if (cards[1] != -1) {
+                        break;
+                    }
+                    continue;
+                }
+                nr[1] = newNr; int keepCard2 = newNr;
+                suit[1] = newSuit; int keepSuit2 = newSuit;
+                for (int card3 = 0; card3 < 52 || cards[2] != -1; card3++) {
+                    newNr = cards[2] != -1 ? cards[2] : card3 / 4;
+                    newSuit = suits[2] != -1 ? suits[2] : card3 % 4;
+                    if (duplicate(newNr, newSuit, nr, suit) || (cards[2] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                        if (cards[2] != -1) {
+                            break;
+                        }
+                        continue;
+                    }
+                    nr[2] = newNr;
+                    suit[2] = newSuit;
+                    for (int card4 = 0; card4 < 52 || cards[3] != -1; card4++) {
+                        if (cards[3] == -1 && cards[2] == -1 && card4 <= card3) {
+                            continue;
+                        }
+                        newNr = cards[3] != -1 ? cards[3] : card4 / 4;
+                        newSuit = suits[3] != -1 ? suits[3] : card4 % 4;
+                        if (duplicate(newNr, newSuit, nr, suit) || (cards[3] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                            if (cards[3] != -1) {
+                                break;
+                            }
+                            continue;
+                        }
+                        nr[3] = newNr;
+                        suit[3] = newSuit;
+                        for (int card5 = 0; card5 < 52 || cards[4] != -1; card5++) {
+                            if (cards[4] == -1 && cards[3] == -1 && card5 <= card4) {
+                                continue;
+                            }
+                            newNr = cards[4] != -1 ? cards[4] : card5 / 4;
+                            newSuit = suits[4] != -1 ? suits[4] : card5 % 4;
+                            if (duplicate(newNr, newSuit, nr, suit) || (cards[4] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                                if (cards[4] != -1) {
+                                    break;
+                                }
+                                continue;
+                            }
+                            nr[4] = newNr;
+                            suit[4] = newSuit;
+                            for (int card6 = 0; card6 < 52 || cards[5] != -1; card6++) {
+                                if (cards[5] == -1 && cards[4] == -1 && card6 <= card5) {
+                                    continue;
+                                }
+                                newNr = cards[5] != -1 ? cards[5] : card6 / 4;
+                                newSuit = suits[5] != -1 ? suits[5] : card6 % 4;
+                                if (duplicate(newNr, newSuit, nr, suit) || (cards[5] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                                    if (cards[5] != -1) {
+                                        break;
+                                    }
+                                    continue;
+                                }
+                                nr[5] = newNr;
+                                suit[5] = newSuit;
+                                for (int card7 = 0; card7 < 52 || cards[6] != -1; card7++) {
+                                    if (cards[6] == -1 && cards[5] == -1 && card7 <= card6) {
+                                        continue;
+                                    }
+                                    newNr = cards[6] != -1 ? cards[6] : card7 / 4;
+                                    newSuit = suits[6] != -1 ? suits[6] : card7 % 4;
+                                    if (duplicate(newNr, newSuit, nr, suit) || (cards[6] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                                        if (cards[6] != -1) {
+                                            break;
+                                        }
+                                        continue;
+                                    }
+                                    nr[6] = newNr;
+                                    suit[6] = newSuit;
+
+                                    int score1 = rankPokerHand7(nr, suit);
+
+                                    for (int card8 = 0; card8 < 52 || cards[7] != -1; card8++) {
+                                        newNr = cards[7] != -1 ? cards[7] : card8 / 4;
+                                        newSuit = suits[7] != -1 ? suits[7] : card8 % 4;
+                                        if (duplicate(newNr, newSuit, nr, suit) || (cards[7] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                                            if (cards[7] != -1) {
+                                                break;
+                                            }
+                                            continue;
+                                        }
+                                        nr[0] = newNr;
+                                        suit[0] = newSuit;
+                                        for (int card9 = 0; card9 < 52 || cards[8] != -1; card9++) {
+                                            if (cards[8] == -1 && cards[7] == -1 && card9 <= card8) {
+                                                continue;
+                                            }
+                                            newNr = cards[8] != -1 ? cards[8] : card9 / 4;
+                                            newSuit = suits[8] != -1 ? suits[8] : card9 % 4;
+                                            if ((newNr == keepCard1 && newSuit == keepSuit1) || duplicate(newNr, newSuit, nr, suit) || (cards[8] == -1 && duplicate(newNr, newSuit, cards, suits))) {
+                                                if (cards[8] != -1) {
+                                                    break;
+                                                }
+                                                continue;
+                                            }
+                                            nr[1] = newNr;
+                                            suit[1] = newSuit;
+
+                                            int score2 = rankPokerHand7(nr, suit);
+
+                                            rangeResult.process(score1, score2);
+
+                                            nr[1] = keepCard2;
+                                            suit[1] = keepSuit2;
+                                            if (cards[8] != -1) {
+                                                break;
+                                            }
+                                        }
+                                        nr[0] = keepCard1;
+                                        suit[0] = keepSuit1;
+                                        if (cards[7] != -1) {
+                                            break;
+                                        }
+                                    }
+                                    if (cards[6] != -1) {
+                                        break;
+                                    }
+                                }
+                                nr[6] = -1;
+                                suit[6] = -1;
+                                if (cards[5] != -1) {
+                                    break;
+                                }
+                            }
+                            nr[5] = -1;
+                            suit[5] = -1;
+                            if (cards[4] != -1) {
+                                break;
+                            }
+                        }
+                        nr[4] = -1;
+                        suit[4] = -1;
+                        if (cards[3] != -1) {
+                            break;
+                        }
+                    }
+                    nr[3] = -1;
+                    suit[3] = -1;
+                    if (cards[2] != -1) {
+                        break;
+                    }
+                }
+                nr[2] = -1;
+                suit[2] = -1;
+                if (cards[1] != -1) {
+                    break;
+                }
+            }
+            nr[1] = -1;
+            suit[1] = -1;
+            if (cards[0] != -1) {
+                break;
+            }
+        }
+        return rangeResult;
+    }
+
+    private static boolean duplicate(int card, int suit, int[] cards, int[] suits) {
+        for (int i=0; i<cards.length; i++) {
+            if (card==cards[i] && suit==suits[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static class RangeResult {
+        public long win;
+        public long draw;
+        public long loss;
+
+        public void process(int score1, int score2) {
+            if (score1 > score2) {
+                win++;
+            } else if (score1 < score2) {
+                loss++;
+            } else {
+                draw++;
+            }
+        }
+
+        public String toString() {
+            return "win: " + win + "; loss: " + loss + "; draw: " + draw;
+        }
+    }
 }
